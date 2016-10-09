cf27c2e0e766a73e706e9c0042d551a67930bddf
iamunstoppable <devbrights@gmail.com>
2016-10-08 21:22:24 -0700

    height now dynamically sizes with elastic effect by a 1/2 milisecond duration

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // create an array data values within a given range a random number of times
 var data_ary = [];  // [10, 70, 25, 145, 195, 23, 70, 15, 133, 80, 42, 22, 10, 46, 77, 25, 253, 32, 22, 222];
 // console.log(data_ary.length)
-
 var minimum = 10   // 1  // 10  // 100
 var maximum = 100 // 10 // 100 // 1000 // 10000 // 100000
 // Generating random whole numbers in JavaScript in a specific range
@@ -47,31 +46,53 @@ var svg = d3.select('#bar_graph')                  // targeting an id
             return colors(i);
         })
         .attr('width', xScale.rangeBand())         // now scales
-        .attr('height', function(d) {              // d ~ data, sets the height to the current data
-            return yScale(d);                      // currently all on top of each other, y scaled to a max value
+    // grow animate the chart height for each rect: part 1
+        // .attr('height', function(d) {              // d ~ data, sets the height to the current data
+        //     return yScale(d);                      // currently all on top of each other, y scaled to a max value
+        // })
+        // .attr('x', function(d, i) {                // barSpacing x of barWidth, using data_ary index
+        //     return xScale(i) ;
+        // })
+        // .attr('y', function(d) {                   // barSpacing y of barWidth
+        //     return backgroundHeight - yScale(d);   // begining height starts at the bottom
+        // })
+    // replacing with the following
+        .attr('x', function(d,i) {
+            return xScale(i);
         })
-        .attr('x', function(d, i) {                // barSpacing x of barWidth, using data_ary index
-            return xScale(i) ;
-        })
-        .attr('y', function(d) {                   // barSpacing y of barWidth
-            return backgroundHeight - yScale(d);   // begining height starts at the bottom
-        })
+        .attr('height', 0)
+        .attr('y', backgroundHeight)
+
     // events (like mouseover, mouseout, onhover...) now implimented
     .on('mouseover', function(d) {
         unsetTempColor = this.style.fill;
         d3.select(this)
-            // in the mouseover context the following changes how long each rect changes color and opacity
             // .transition()
-            // .transition().duration(100)          // defaults to 2500ms
-            .transition().delay(10).duration(100)   // defaults to 2500ms, or use delay(10)
+            // .transition().duration(100)            // defaults to 2500ms
+            // .transition().delay(10).duration(100)     // defaults to 2500ms, or use delay(10)
             .style('opacity', .3)
             .style('fill', setTempColor)            // add a color, if desired
     })
     .on('mouseout', function(d) {                   // similarly
         d3.select(this)
-            // .transition()
+            // .transition()                        // defaults to 2500ms, or use delay(10)
             // .transition().duration(100)
-            .transition().delay(40).duration(100)   // defaults to 2500ms, or use delay(10)
+            // .transition().delay(40).duration(100)
             .style('opacity', 1)
             .style('fill', unsetTempColor)          // add a color, if desired
     })
+
+// grow animate the chart height for each rect: part 2
+svg.transition()
+    .attr('height', function(d) {
+        return yScale(d);
+    })
+    .attr('y', function(d) {
+        return backgroundHeight - yScale(d);
+    })
+// was fast so adding a delay for the growing
+    .delay(function(d, i) {
+        return i * 20;
+    })
+    .duration(500)                                  // adds delay
+    .ease('elastic')                                // nice bouncy effect, too long pulls back to short, and vica versa
