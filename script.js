4d9b3d5a8f0ee3f68dcf3eb2126df839f5686ee3
iamunstoppable <devbrights@gmail.com>
2016-10-08 20:44:59 -0700

    events mouseover & mouseout implimenting opacity and changing colors

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 // create an array data values within a given range a random number of times
 var data_ary = [];  // [10, 70, 25, 145, 195, 23, 70, 15, 133, 80, 42, 22, 10, 46, 77, 25, 253, 32, 22, 222];
 var minimum = 10   // 1  // 10  // 100
-var maximum = 1000 // 10 // 100 // 1000 // 10000 // 100000
+var maximum = 100 // 10 // 100 // 1000 // 10000 // 100000
 // Generating random whole numbers in JavaScript in a specific range
 var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-// console.log(randomnumber)
 
 // each refresh gives new values
 for (var i=0; i < randomnumber; i++) { data_ary.push(Math.random()) }
-// for (var i=0; i < 100; i++) { data_ary.push(Math.random()) }
 
 var backgroundHeight = 400,
     backgroundWidth = 600,
     barWidth = 20,
-    barSpacing = 10;
+    barSpacing = 10,
+    setTempColor = '#f6f';
+    unsetTempColor = null;
 
 // change color of chart bars depending on horizontal position
 var colors = d3.scale.linear()
-    // .domain([0, data_ary.length*.33, data_ary.length*.66, data_ary.length])
     .domain([0, data_ary.length*(1/3), data_ary.length*(2/3), data_ary.length])
     .range(['yellow', 'orange', 'red', 'purple'])
-    // .domain([0, d3.max(data_ary)]) // if by height
-    // .range(['aqua', 'red'])        // if hard coded
 
 // linear scaling y
 // to make sure all data fits into the chart as the array grows in the y direction
@@ -44,8 +41,6 @@ var svg = d3.select('#bar_graph')                  // targeting an id
         .data(data_ary)                            // coming from selectAll rect, data will be the y axis
         .enter()                                   // switch to yet-to-be-added elements selection
     .append('rect')                                // ~ bars, as we go through the data_ary we append a rect
-        // .style('fill', 'aqua')                     // if hard coded, style the rect color
-        // .style('fill', colors)                     // if by height
         .style('fill', function(d,i) {             // if by
             return colors(i);
         })
@@ -55,10 +50,21 @@ var svg = d3.select('#bar_graph')                  // targeting an id
         })
         .attr('x', function(d, i) {                // barSpacing x of barWidth, using data_ary index
             return xScale(i) ;
-            // return i * (barWidth + barSpacing);    // each x (width + spacing) * total index to fit in x axis
         })
         .attr('y', function(d) {                   // barSpacing y of barWidth
             return backgroundHeight - yScale(d);   // begining height starts at the bottom
         })
+    // events (like mouseover, mouseout, onhover...) now implimented
+    .on('mouseover', function(d) {
+        unsetTempColor = this.style.fill;
+        d3.select(this)
+            .style('opacity', .3)
+            .style('fill', setTempColor)            // add a color, if desired
+    })
+    .on('mouseout', function(d) {                   // similarly
+        d3.select(this)
+            .style('opacity', 1)
+            .style('fill', unsetTempColor)          // add a color, if desired
+    })
 
 console.log(data_ary.length)
