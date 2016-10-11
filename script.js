3af427a0a810589594412a98c785b41a10cb7544
iamunstoppable <devbrights@gmail.com>
2016-10-11 16:44:22 -0700

    scales positioned

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 // create an array data values within a given range a random number of times
-var dataAry = [],  // [10, 70, 25, 145, 195, 23, 70, 15, 133, 80, 42, 22, 10, 46, 77, 25, 253, 32, 22, 222];
+var dataAry = [],   // [10, 70, 25, 145, 195, 23, 70, 15, 133, 80, 42, 22, 10, 46, 77, 25, 253, 32, 22, 222];
     minimum = 10,   // 1  // 10  // 100
-    maximum = 100;  // 10 // 100 // 1000 // 10000 // 100000
-
+    maximum = 100,  // 10 // 100 // 1000 // 10000 // 100000
 // Generating random whole numbers in JavaScript in a specific range
-var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+    randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum,
+// random number of times
+    times   = Math.floor(Math.random() * 60) + 10, // *maximum + minimum);
+// round to the nearest 10
+    rounded   = Math.round(randomnumber/ 10) * 10;
+// set times now to that rounded amount to avoid the x scale failing to appear
+    times   = rounded
+    // console.log(randomnumber)
+    // console.log(times)
 
 // each refresh gives new values
-for (var i=0; i < maximum; i++) {
-    dataAry.push(Math.round(Math.random()*maximum)+minimum) // random whole numbers, within given range
+for (var i=0; i < times; i++) {
+    // random whole numbers, within given range
+    dataAry.push(Math.round(Math.random()*maximum)+minimum)
 }
 
-// var margin = { top: -20, right: 30, bottom: 20, left:50 }
-var margin = { top: -30, right: 0, bottom: 0, left:30 }
+var margin = { top: 30, right: 30, bottom: 40, left:50 }
 
-
-var backgroundHeight = 400,
-    backgroundWidth = 600,
+var backgroundHeight = 400 - margin.top - margin.bottom,
+    backgroundWidth = 600 - margin.left - margin.right,
     barWidth = 20,
     barSpacing = 10,
     setTempColor = '#f6f';                     // pinkish
@@ -25,7 +31,7 @@ var backgroundHeight = 400,
 
 // change color of chart bars depending on horizontal position
 var colors = d3.scale.linear()
-    .domain([0, dataAry.length*(1/3), dataAry.length*(2/3), dataAry.length])
+    .domain([0, dataAry.length*(.33), dataAry.length*(.66), dataAry.length])
     .range(['yellow', 'orange', 'red', 'purple'])
 
 // linear scaling y
@@ -57,8 +63,12 @@ var showDataValue = d3.select('body')
 var svg = d3.select('#bar_graph')                  // targeting an id
     .append('svg')                                 // create <svg> tag
         .style('background', backgroundColor)      // style background color
-        .attr('height', backgroundHeight)          // set background height
-        .attr('width', backgroundWidth)            // set background width
+        // .attr('height', backgroundHeight)          // set background height
+        // .attr('width', backgroundWidth)            // set background width
+        .attr('width', backgroundWidth + margin.left + margin.right)   // adding margins
+        .attr('height', backgroundHeight + margin.top + margin.bottom) // ditto
+    .append('g')                                                       // appending a new element // BEST: now can just moving the #bar_graph to where the scale is
+        .attr('transform', 'translate('+ margin.left +', '+ margin.top +')')
         .selectAll('rect')                         // svg <rect> are the bars in the graph
         .data(dataAry)                            // coming from selectAll rect, data will be the y axis
         .enter()                                   // switch to yet-to-be-added elements selection
